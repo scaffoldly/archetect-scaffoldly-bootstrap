52ac46aca98309965adaba53886ee0534d49ef6a
Christian Nuss <christian.nuss@gmail.com>
2021-08-09 12:47:05 -0700

    Remove workflow files

--- contents/variables.tf
+++ contents/variables.tf
@@ -3,6 +3,11 @@ variable "ROOT_EMAIL" {
   description = "Root email address"
 }
 
+variable "BOOTSTRAP_AWS_ACCOUNT_ID" {
+  type        = string
+  description = "AWS Account ID in which to create resources"
+}
+
 variable "BOOTSTRAP_GITHUB_TOKEN" {
   type        = string
   description = "GitHub Token"
