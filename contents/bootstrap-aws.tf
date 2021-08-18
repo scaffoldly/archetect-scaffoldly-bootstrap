096b70f77e4fec45201c60f27e7151a824856e57
Christian Nuss <christian.nuss@gmail.com>
2021-08-18 15:50:49 -0700

    scaffoldly/bootstrap/aws 1.0.3

--- contents/bootstrap-aws.tf
+++ contents/bootstrap-aws.tf
@@ -8,7 +8,7 @@ variable "BOOTSTRAP_AWS_ACCOUNT_ID" {
 
 module "bootstrap_aws" {
   source  = "scaffoldly/bootstrap/aws"
-  version = "1.0.2"
+  version = "1.0.3"
 
   root_email   = var.ROOT_EMAIL
   account_id   = var.BOOTSTRAP_AWS_ACCOUNT_ID
