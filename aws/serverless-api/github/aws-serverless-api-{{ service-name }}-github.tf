a71ad093923d4f5f9c7074e4392d542d3e646c69
Christian Nuss <christian.nuss@gmail.com>
2021-08-27 08:57:08 -0700

    Update module reference

--- aws/serverless-api/github/aws-serverless-api-{{ service-name }}-github.tf
+++ aws/serverless-api/github/aws-serverless-api-{{ service-name }}-github.tf
@@ -12,8 +12,8 @@ module "github_stage_secrets_{{ service_name }}" {
   secrets = {
     AWS_PARTITION                 = data.aws_partition.current.partition
     AWS_ACCOUNT_ID                = data.aws_caller_identity.current.account_id
-    AWS_ACCESS_KEY_ID             = module.aws_iam.deployer_access_key
-    AWS_SECRET_ACCESS_KEY         = module.aws_iam.deployer_secret_key
+    AWS_ACCESS_KEY_ID             = module.aws_serverless_api_{{ service_name }}.deployer_credentials.access_key
+    AWS_SECRET_ACCESS_KEY         = module.aws_serverless_api_{{ service_name }}.deployer_credentials.secret_key
     AWS_REST_API_ID               = each.value.api_id
     AWS_REST_API_ROOT_RESOURCE_ID = each.value.root_resource_id
   }
