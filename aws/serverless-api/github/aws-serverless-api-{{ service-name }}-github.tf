cd6df4aca176b4714fee5cf75df4c00045670c47
Christian Nuss <christian.nuss@gmail.com>
2021-08-27 09:06:56 -0700

    Update Variable Name

--- aws/serverless-api/github/aws-serverless-api-{{ service-name }}-github.tf
+++ aws/serverless-api/github/aws-serverless-api-{{ service-name }}-github.tf
@@ -15,7 +15,7 @@ module "github_stage_secrets_{{ service_name }}" {
     AWS_ACCESS_KEY_ID             = module.aws_serverless_api_{{ service_name }}.deployer_credentials.access_key
     AWS_SECRET_ACCESS_KEY         = module.aws_serverless_api_{{ service_name }}.deployer_credentials.secret_key
     AWS_REST_API_ID               = each.value.api_id
-    AWS_REST_API_ROOT_RESOURCE_ID = each.value.root_resource_id
+    AWS_REST_API_ROOT_RESOURCE_ID = each.value.api_resource_id
   }
 
   depends_on = [
