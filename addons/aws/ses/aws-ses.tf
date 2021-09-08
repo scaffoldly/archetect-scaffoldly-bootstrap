3d7cad19e5651e32e0c1b4187a4b1afc143abf02
Christian Nuss <christian.nuss@gmail.com>
2021-09-07 17:01:09 -0700

    KMS Keys per Stage

--- addons/aws/ses/aws-ses.tf
+++ addons/aws/ses/aws-ses.tf
@@ -1,12 +1,12 @@
 module "aws_ses" {
   source  = "scaffoldly/email/aws"
-  version = "1.0.2"
+  version = "1.0.6"
 
   root_email = var.ROOT_EMAIL
   account_id = var.BOOTSTRAP_AWS_ACCOUNT_ID
 
-  subdomain = "slyses"
-  stages    = module.bootstrap_aws.stages
+  subdomain          = "slyses"
+  api_gateway_stages = module.bootstrap_aws.api_gateway_stages
 
   providers = {
     aws.dns = aws.root
