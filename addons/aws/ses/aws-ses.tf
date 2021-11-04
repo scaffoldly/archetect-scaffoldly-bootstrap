8076e733d7d6d1ed60c907bbd7a6c6ebafcb9419
Christian Nuss <christian.nuss@gmail.com>
2021-11-04 10:23:44 -0700

    scaffoldly/email/aws 1.0.7
     - Output bootstrap_aws_ses_mail_stages

--- addons/aws/ses/aws-ses.tf
+++ addons/aws/ses/aws-ses.tf
@@ -1,6 +1,6 @@
 module "aws_ses" {
   source  = "scaffoldly/email/aws"
-  version = "1.0.6"
+  version = "1.0.7"
 
   root_email = var.ROOT_EMAIL
   account_id = var.BOOTSTRAP_AWS_ACCOUNT_ID
@@ -15,4 +15,8 @@ module "aws_ses" {
   depends_on = [
     module.bootstrap_aws
   ]
+}
+
+output "bootstrap_aws_ses_mail_stages" {
+  value = module.aws_ses.mail_stages
 }
