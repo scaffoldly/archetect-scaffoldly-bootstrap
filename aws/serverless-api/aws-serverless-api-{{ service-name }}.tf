8d4abcceb97fcd73f7bd410d81056a8c0158841d
Christian Nuss <cnuss@users.noreply.github.com>
2022-12-05 08:11:39 -0800

    scaffoldly/serverless-api/aws@1.0.41

--- aws/serverless-api/aws-serverless-api-{{ service-name }}.tf
+++ aws/serverless-api/aws-serverless-api-{{ service-name }}.tf
@@ -2,7 +2,7 @@
 
 module "aws_serverless_api_{{ service_name }}" {
   source  = "scaffoldly/serverless-api/aws"
-  version = "1.0.40"
+  version = "1.0.41"
 
   repository_name    = "{{ repository-name }}"
   api_gateway_stages = module.bootstrap_aws.api_gateway_stages
