9186da2054a90aac6f27994fd521fac9be907a98
Christian Nuss <cnuss@users.noreply.github.com>
2022-12-05 02:47:09 -0800

    scaffoldly/serverless-api/aws@1.0.39

--- aws/serverless-api/aws-serverless-api-{{ service-name }}.tf
+++ aws/serverless-api/aws-serverless-api-{{ service-name }}.tf
@@ -2,13 +2,14 @@
 
 module "aws_serverless_api_{{ service_name }}" {
   source  = "scaffoldly/serverless-api/aws"
-  version = "1.0.38"
+  version = "1.0.39"
 
   repository_name    = "{{ repository-name }}"
   api_gateway_stages = module.bootstrap_aws.api_gateway_stages
   path               = "{{ path }}"
 
   kms_stages = module.bootstrap_aws.kms_stages
+  saml_trust = module.bootstrap_aws.saml_to_trust
 
   depends_on = [
     module.bootstrap_aws
